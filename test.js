e2c05c2cb64f06eab1814416eb950ac841724ada
arty <art.yerkes@gmail.com>
2013-08-31 03:26:43 -0700

    Add test cases for bugs found by mhoffesommer

--- test.js
+++ test.js
@@ -5,3 +5,10 @@ clog('[Array] an array looks like ' + (Array('a','b','c')));
 clog('[Function] a function looks like ' + (function(a,b) { clog(a + b); }));
 clog('[Math] PI is ' + Math.PI);
 clog('[Stdlib] decode uri: (want !) ' + decodeURI('%21'));
+var a = {'x':3, 'y':2, 'toString':function() { return '[' + this.x + ',' + this.y + ']'; }};
+var a_str;
+switch (a.toString()) {
+	case '[3,2]': a_str = 'case correct'; break;
+	default: a_str = 'case wrong'; break;
+}
+clog(a_str + ': a = ' + a.toString());
